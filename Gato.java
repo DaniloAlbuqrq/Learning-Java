b3a833ad5f8ec7aecc4040e18271b0c9790a1576
Danilo Albuquerque <95035311+DaniloAlbuqrq@users.noreply.github.com>
2021-12-07 11:19:14 -0300

    Add files via upload

---
+++ Gato.java
@@ -0,0 +1,22 @@
+
+package gato;
+
+/**
+ *
+ * @author Danilo
+ */
+public class Gato {
+    
+     
+    public static void main(String[] args) {
+        String cat;
+        int gatinhos = 0;
+        System.out.println("Olá Mundo! Eu tenho: " +gatinhos);
+        System.out.println("Olá Mundo! Eu sou um gato!");
+         if (gatinhos>5) {
+             System.out.println("OláA quantidade de gatinhos é: ");
+         }
+    
+    
+}
+}

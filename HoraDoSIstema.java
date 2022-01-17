bd497ab78040a071ed95d54f6a18e6ad0038e908
Danilo Albuquerque <95035311+DaniloAlbuqrq@users.noreply.github.com>
2022-01-17 08:56:23 -0300

    Add files via upload

---
+++ HoraDoSIstema.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horadosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Danilo
+ */
+public class HoraDoSIstema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Date relogio = new Date ();
+        System.out.println("A hora do sistema é");
+        System.out.println(relogio.toString());
+        
+    }
+    
+}

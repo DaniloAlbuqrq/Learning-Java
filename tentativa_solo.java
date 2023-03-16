4dcbfc27eae1cd756e73db593b383a8146d8c109
Danilo Albuquerque <95035311+DaniloAlbuqrq@users.noreply.github.com>
2023-03-16 07:50:18 -0300

    Add files via upload

---
+++ tentativa_solo.java
@@ -0,0 +1,29 @@
+package aplication;
+import entities.tenta;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class tentativa_solo {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a;
+        System.out.println("Entre o tipo de valor do seu seguro: ");
+        a = sc.nextLine();
+
+        String b;
+        System.out.println("Entre com o tipo do seu seguro 2: ");
+        b = sc.nextLine();
+
+        String c;
+        System.out.println("Entre com o tipo do seu seguro 3");
+        c = sc.nextLine();
+
+
+        tenta tentativ = new tenta();
+
+
+        }
+    }
+
